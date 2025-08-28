73cc5198935a6c8c45b8ddf170bcc0e75a3167be
Yenhi6886 <nguyenyenhi220904@gmail.com>
2025-08-28 16:46:09 +0700

    Update route remove basename

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter, HashRouter } from "react-router-dom";
 // Tạo root React từ element có id="root" trong HTML và render ứng dụng
 ReactDOM.createRoot(document.getElementById("root")).render(
   // BrowserRouter bao bọc App để enable client-side routing
-  <HashRouter basename="/yenhi6886">
+  <HashRouter>
     {/* App component chính chứa toàn bộ ứng dụng */}
     <App />
   </HashRouter>
