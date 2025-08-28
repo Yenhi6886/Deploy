43ce115b2c3a0433562c363cd6c3a88892acce9b
Yenhi6886 <nguyenyenhi220904@gmail.com>
2025-08-28 16:41:33 +0700

    Update route

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 // Import App component chính
 import App from "./App";
 // Import BrowserRouter từ react-router-dom để enable routing
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 // Tạo root React từ element có id="root" trong HTML và render ứng dụng
 ReactDOM.createRoot(document.getElementById("root")).render(
   // BrowserRouter bao bọc App để enable client-side routing
-  <BrowserRouter>
+  <HashRouter basename="/yenhi6886">
     {/* App component chính chứa toàn bộ ứng dụng */}
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
